9a08ebe7d8e6b69abdaced2db27d362080e907c8
crtaylor15 <144953767+crtaylor15@users.noreply.github.com>
2023-12-03 15:20:04 -0600

    Update Member.java
    ported over from old member class

--- src/main/java/com/example/demo/Member.java
+++ src/main/java/com/example/demo/Member.java
@@ -1,9 +1,6 @@
-package com.example.demo;
-
 public class Member {
     private String name;
     private String number; // 9-digit member number
-    private String email;
     private String streetAddress;
     private String city;
     private String state; // 2-letter state code
@@ -11,80 +8,99 @@ public class Member {
     private boolean status; // Active or suspended
 
     // Constructor
-    public Member(String memberName, String memberNumber, String memberEmail, String memberStreetAddress, String memberCity, String memberState, String memberZipCode) {
-        this.name = memberName;
-        this.number = memberNumber;
-        this.email = memberEmail;
-        this.streetAddress = memberStreetAddress;
-        this.city = memberCity;
-        this.state = memberState;
-        this.zipCode = memberZipCode;
+    public Member(String name, String number, String streetAddress, String city, String state, String zipCode) {
+        this.name = name;
+        this.number = number;
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
         this.status = true; // Assuming new members are active by default
     }
 
-    public boolean changeName(String newName) {
-        name = newName;
-        return true;
-    }
-
-    // Getters and Setters
+    // Getters
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getNumber() {
         return number;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getStreetAddress() {
         return streetAddress;
-    }
-
-    public void setStreetAddress(String streetAddress) {
-        this.streetAddress = streetAddress;
     }
 
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
+        return state.toUpperCase();
     }
 
     public String getZipCode() {
         return zipCode;
     }
 
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
+    public boolean getStatus() {
+        return status;
     }
 
-    public boolean isStatus() {
-        return status;
+    // Setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumber(String number) {
+        if (number.length() != 9) {
+            System.out.println("Member number must be 9 digits");
+            return;
+        }
+        this.number = number;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public void setCity(String city) {
+        if (city.length() > 20) {
+            System.out.println("City name must be less than 20 characters");
+            return;
+        }
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        if (state.length() != 2) {
+            System.out.println("State code must be 2 characters");
+            return;
+        }
+        this.state = state;
+    }
+
+    public void setZipCode(String zipCode) {
+        if (zipCode.length() != 5) {
+            System.out.println("ZIP code must be 5 digits");
+            return;
+        }
+        this.zipCode = zipCode;
     }
 
     public void setStatus(boolean status) {
         this.status = status;
+    }
+
+    // Other methods
+
+    public String toString() {
+        return "Name: " + name + "\nNumber: " + number + "\nStreet Address: " + streetAddress + "\nCity: " + city
+                + "\nState: " + state + "\nZIP Code: " + zipCode + "\nStatus: " + (status ? "Active" : "Suspended");
+    }
+
+    public boolean equals(Member member) {
+        return this.number.equals(member.getNumber());
     }
 }
